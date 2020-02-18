a8fa36cae436e2a84e10a5242b32b8a4d60c44c3
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-18 22:46:55 +0200

    ngForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f')
+  @ViewChild('f') signupForm: NgForm;
 
   defaultSubscription = 'advanced';
 

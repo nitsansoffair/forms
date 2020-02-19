469714c6c4e796055aeda26e7c4a4f59e59da079
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-19 17:54:29 +0200

    FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      projectName: new FormControl(null),
+      email: new FormControl(null),
+      projectStatus: new FormControl(null)
+    });
+  }
 
   onSubmit() {}
 }

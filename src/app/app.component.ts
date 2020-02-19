95d4884e2e598aef891639dce116c24f967b8012
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-02-19 17:45:06 +0200

    reacting to value or status changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,10 @@ export class AppComponent implements OnInit {
       gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
+
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log(status);
+    });
   }
 
   onSubmit() {
